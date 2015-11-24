9338b9931535b9e2133276cfd76fb71e1e95d89e
francesc <francesc.guillen@gmail.com>
2015-11-24 14:58:28 +0100

    setup cljs test with doo

--- project.clj
+++ project.clj
@@ -2,5 +2,26 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+            :url "http://www.eclipse.org/legal/epl-v10.HTML"}
+
+  :plugins [[lein-cljsbuild "1.1.1"]
+            [lein-doo "0.1.4"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]]
+
+  :source-paths ["src" "test"]
+
+  :clean-targets ^{:protect false} [:target-path "resources/public/js/" "out"]
+
+  :doo {:build "test"
+        :paths {:slimer "./node_modules/.bin/slimerjs"}
+        :alias {:browsers [:chrome :firefox]
+                :all [:browsers :headless]}}
+
+  :cljsbuild
+  { :builds {:test {:source-paths ["src" "test"]
+                    :compiler     {:output-to     "out/testable.js"
+                                   :main          'mema.runner
+                                   :optimizations :simple}}}
+   }
+  )
--- test/mema/core_test.cljc
+++ test/mema/core_test.cljc
@@ -1,7 +1,8 @@
 (ns mema.core-test
-  (:require [clojure.test :refer :all]
-            [mema.core :refer :all]))
+  (:require #?(:clj [clojure.test :as t]
+              :cljs [cljs.test :as t :include-macros true])
+            [mema.core :as sut]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(t/deftest a-test
+  (t/testing "FIXME, I fail."
+    (t/is (= 2 1))))
